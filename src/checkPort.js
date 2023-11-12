9da1debcb7d66c5ea8811d3dfc406f310f3b47d0
Jackson Rico <jacksonrico@gmail.com>
2023-11-11 22:00:22 -0500

    feat: desafio entrega servidor express

---
+++ src/checkPort.js
@@ -0,0 +1,26 @@
+import net from 'net';
+
+function checkPort(port) {
+  return new Promise((resolve, reject) => {
+    const server = net
+        .createServer()
+        .once('error', (err) => {
+            // @ts-ignore
+            if (err.code === 'EADDRINUSE') {
+                resolve(true);
+            } else {
+                reject(err);
+            }
+        })
+        .once('listening', () => {
+        server
+          .once('close', () => {
+            resolve(false);
+          })
+          .close();
+      })
+      .listen(port);
+  });
+}
+
+export default checkPort;
