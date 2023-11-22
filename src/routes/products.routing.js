a81f9ae16ec79a854a4e4be40cbcf04ebad06eaa
Jackson Rico <jacksonrico@gmail.com>
2023-11-22 05:05:02 -0500

    Remove unnecessary product from database and add POST route for creating products

--- src/routes/products.routing.js
+++ src/routes/products.routing.js
@@ -4,8 +4,8 @@ import { controller } from '../controller/productsController.js';
 export const ProductRouter = Router();
 
 ProductRouter.get('/', controller.get);
+ProductRouter.post('/', controller.post);
 ProductRouter.get('/products/:id', controller.getById);
 ProductRouter.get('/products', controller.getProducts);
-ProductRouter.post('/product', controller.post);
 
 // export default ProductRouter;
