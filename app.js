104b5209c2e37fd5fed3becb1e632c3cc8ba46bf
Jackson Rico <jacksonrico@gmail.com>
2024-01-03 00:58:05 -0500

    Refactor database connection code and add error handling

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 // author: Jackson Rico
 import Server from './src/config/Server.js';
 import dotenv from 'dotenv';
-// import 'dotenv/config';
-dotenv.config();
 
+
+dotenv.config();
 const server = new Server();
 server.start();
--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,44 @@
 // config/database.js
 import mongoose from 'mongoose';
+import process from 'process';
 
-export const dbConnection = (uri) => {
-    //MONGOOSE CONNECTION
-    const db = mongoose.connection;
+export const dbConnection = async (uri) => {
+    try {
+        await mongoose.connect(uri, {});
+        console.log('Connected to MongoDB');
 
-    mongoose.connect(uri, {});
-    //CONNECTION EVENTS
-    db.on('connected', () => {
-        console.log(`You are connected to Mongo`);
-    })
-        .on('error', (error) => {
-            console.error(error.message);
-        })
-        .on('disconnected', () => {
-            console.log('Mongoose Disconnected');
+        const db = mongoose.connection;
+
+        //CONNECTION EVENTS
+        db.on('connected', () => {
+            console.log(`You are connected to Mongo ${db.name} at ${db.host}:${db.port}`);
         });
+        // Event handling for connection error
+        db.on('error', (err) => {
+            console.log(`Database connection error: ${err}`);
+        });
+        // Event handling for connection disconnect
+        db.on('disconnected', () => {
+            console.log('MongoDB disconnected');
+        });
+
+        // Close the connection when done
+        process.on('SIGINT', async () => {
+            await mongoose.connection.close();
+            console.log('MongoDB connection closed');
+            process.exit(0);
+        });
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+
+        // Handle specific error conditions
+        if (error.name === 'MongoNetworkError') {
+            console.error('Network error occurred. Check your MongoDB server.');
+        } else if (error.name === 'MongooseServerSelectionError') {
+            console.error('Server selection error. Ensure MongoDB is running and accessible.');
+        } else {
+            // Handle other types of errors
+            console.error('An unexpected error occurred:', error);
+        }
+    }
 };
