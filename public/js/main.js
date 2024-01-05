5ee2507809f51f5d1b87d1a775b094d1c3193bae
Jackson Rico <jacksonrico@gmail.com>
2024-01-05 06:42:41 -0500

    Add styling for header and hero section, and implement modal functionality

--- public/js/main.js
+++ public/js/main.js
@@ -34,3 +34,11 @@ for (let i = 0; i < delElements.length; i++) {
             });
     });
 }
+
+// document.getElementById('button').addEventListener("click", function() {
+// 	document.querySelector('.bg-modal').style.display = "flex";
+// });
+
+// document.querySelector('.close').addEventListener("click", function() {
+// 	document.querySelector('.bg-modal').style.display = "none";
+// });
